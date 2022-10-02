ed94fbd320fc1a4a2e3ae7aaa45fa9728f42882a
jorgealbertojc <219255+jorgealbertojc@users.noreply.github.com>
2022-10-02 14:27:40 -0500

    chore(added):configuration for migration

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 BACKUP_LOG_FILEPATH=/tmp/$(date +'%F-%s.%3N')-$(basename $(pwd)).log
 # BKPDIR_DEBUG=yes
 
-bash ./cmd/bkpdir/main.sh ./configs/2022-09-backing-pup-multimedia-disk-directories/config.yml >> ${BACKUP_LOG_FILEPATH} 2>&1 &
+bash ./cmd/bkpdir/main.sh ${1} >> ${BACKUP_LOG_FILEPATH} 2>&1 &
 disown
 
 PROCESS_UUID=$(ps xa | grep 'bkpdir' | grep -v 'grep' | awk '{print $1}')
